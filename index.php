2c9afe564cad1664e1057330aae0bdb6d2725f2e
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-05-02 02:07:33 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://nubecolectiva.com/favicon.ico" />
+
+    <meta name="theme-color" content="#000000" />
+
+    <title>Cargar una ubicación desde la Base de Datos MySQL con PHP y Google Maps Javascript API </title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style type="text/css">
+      #mapa {
+            height: 50vh;
+      }
+      .h2s {
+        font-size: 3vh;
+      }
+    </style>          
+
+  </head>
+
+  <body> 
+
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="http://nubecolectiva.com"><img src="http://nubecolectiva.com/img/logo.png" class="img-fluid"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="http://nubecolectiva.com">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://blog.nubecolectiva.com" target="_blank">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> 
+          </ul>
+          <form name="bencc" method="get" action="http://www.google.com/search" id="bencc" class="form-inline mt-2 mt-md-0" target="_blank">
+            <input type="hidden" name="domains" value="blog.nubecolectiva.com">
+            <input type="hidden" name="sitesearch" value="blog.nubecolectiva.com">
+            <input class="form-control mr-sm-2" type="text" placeholder="Buscar..." aria-label="Buscar...">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="document.getElementById('bencc').submit()">Buscar</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    </header>
+
+    <div class="pccp mt-5 mb-3" align="center">
+              <!-- P -->
+              <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              
+              <ins class="adsbygoogle"
+                   style="display:block"
+                   data-ad-client="ca-pub-2390065838671224"
+                   data-ad-slot="1441100372"
+                   data-ad-format="auto"
+                   data-full-width-responsive="true"></ins>
+              <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
+              <!-- End P -->
+    </div>
+
+    <main role="main">
+
+        <div class="container text-center mt-5">
+
+          <div class="row">
+
+            <div class="col-md-12">
+
+              <h1 class="mb-4">Cargar una ubicación desde la Base de Datos MySQL con PHP y Google Maps Javascript API </h1>
+
+              <p>"Coloca el puntero del Mouse en cada marcador rojo del Mapa para ver la dirección correspondiente"</p>
+
+              <p>
+                <strong>Nota:</strong><br>
+                El mapa, marcadores y direcciones pueden dar errores de visualización por restricciones de haber excedido la quota diaria gratis de la API de Google Maps Javascript </p>
+
+              <!-- Contenedor del Mapa de Google --> 
+              <div id="mapa"></div>               
+
+            </div>
+
+          </div>
+
+          <div class="row mt-3">
+
+            <div class="col-md-12">
+
+              <h2 class="h2s">Direcciónes en la Base de Datos MySQL</h2>
+
+              <!-- Archivo PHP con la lógica para mostrar una tabla con las ubicaciones -->
+              <?php include('php/app.php'); ?> 
+              
+            </div>
+            
+          </div>  
+
+
+          <hr>
+
+          <div class="row">
+
+            <div class="col-md-12">
+
+              <p class="lead">En <a href="http://nubecolectiva.com/" target="_blank"> Nube Colectiva </a> hablamos sobre:</p>
+
+            </div>
+
+          </div>
+
+          <div class="row">
+
+              <div class="col-md-3">
+                <h3>Frontend</h3>
+                <a href="http://blog.nubecolectiva.com/category/frontend/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-11-300x169.png">
+                </a>
+              </div>
+
+              <div class="col-md-3">
+                <h3>Backend</h3>                
+                <a href="http://blog.nubecolectiva.com/category/backend/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-8-300x169.png">
+                </a>
+              </div>              
+
+              <div class="col-md-3">
+                <h3>Android</h3>
+                <a href="http://blog.nubecolectiva.com/category/android/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-9-300x169.png">
+                </a>
+              </div>
+
+              <div class="col-md-3">
+                <h3>Otros</h3>
+                <a href="http://blog.nubecolectiva.com/category/articulos/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/09/edit_img_destacada_blog_devs-300x169.png">
+                </a>
+              </div>
+
+          </div>           
+          
+          
+        </div>
+
+    </main>
+
+
+    <footer class="text-muted mt-3 mb-3">
+        <div align="center">
+          Desarrollado por <a href="http://www.nubecolectiva.com" target="_blank">Nube Colectiva</a>
+      </div> 
+    </footer>    
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqKNTA72e78Rcd2ucHZ_YamoEeF6e-fJE&callback=initMap"></script>
+
+    <script type="text/javascript">
+      function initMap() {
+          var map;
+          var bounds = new google.maps.LatLngBounds();
+          var mapOptions = {
+              mapTypeId: 'roadmap'
+          };
+
+          map = new google.maps.Map(document.getElementById('mapa'), {
+              mapOptions
+          });
+
+          map.setTilt(50);
+
+          // Crear múltiples marcadores desde la Base de Datos 
+          var marcadores = [
+              <?php include('php/marcadores.php'); ?>
+          ];
+
+          // Creamos la ventana de información para cada Marcador
+          var ventanaInfo = [
+              <?php include('php/info_marcadores.php'); ?>
+          ];
+
+          // Creamos la ventana de información con los marcadores 
+          var mostrarMarcadores = new google.maps.InfoWindow(),
+              marcadores, i;
+
+          // Colocamos los marcadores en el Mapa de Google 
+          for (i = 0; i < marcadores.length; i++) {
+              var position = new google.maps.LatLng(marcadores[i][1], marcadores[i][2]);
+              bounds.extend(position);
+              marker = new google.maps.Marker({
+                  position: position,
+                  map: map,
+                  title: marcadores[i][0]
+              });
+
+              // Colocamos la ventana de información a cada Marcador del Mapa de Google 
+              google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                  return function() {
+                      mostrarMarcadores.setContent(ventanaInfo[i][0]);
+                      mostrarMarcadores.open(map, marker);
+                  }
+              })(marker, i));
+
+              // Centramos el Mapa de Google para que todos los marcadores se puedan ver 
+              map.fitBounds(bounds);
+          }
+
+          // Aplicamos el evento 'bounds_changed' que detecta cambios en la ventana del Mapa de Google, también le configramos un zoom de 14 
+          var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+              this.setZoom(14);
+              google.maps.event.removeListener(boundsListener);
+          });
+
+      }
+
+      // Lanzamos la función 'initMap' para que muestre el Mapa con Los Marcadores y toda la configuración realizada 
+      google.maps.event.addDomListener(window, 'load', initMap);
+    </script>
+    
+  </body>
+</html>
